5cbd17bb630b10edbe54ea4b09c84571b1319aba
Nacho <nachogutmann@gmail.com>
2022-08-04 21:24:37 -0300

    Restart and Change Size

--- site.js
+++ site.js
@@ -1,6 +1,5 @@
 function start(length){
-    const container = document.querySelector('.grid-container');
-    container.classList.add('test');
+    container.style.gridTemplateColumns = 'repeat(' + Math.sqrt(length) + ', auto)';
     for(let i = 0; i < length; i++){
         let divs = document.createElement('div');
         divs.classList.add('grid-item');
@@ -19,8 +18,23 @@ function start(length){
     let color = '#000000';
     colorPicker.addEventListener("change", () => {
         color = event.target.value;
+        
     }, false);
 }
 
-const defaultLength = 256;
-start(defaultLength);
+const container = document.querySelector('.grid-container');
+const length = document.querySelector('#size');
+let lengthSelected = size.value * size.value;
+start(lengthSelected);
+
+function restartFunction() {
+    console.log(lengthSelected);
+    container.textContent = '';
+    start(lengthSelected);
+}
+
+function updateSizePicker(val) {
+    document.getElementById('number').textContent=val + " x " + val; 
+    lengthSelected = val * val;
+    restartFunction();
+  }
